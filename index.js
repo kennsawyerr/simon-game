0c2b7489f98c807817e8e9ec8131334fdde41ab4
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-01 17:08:33 +0100

    changed the running of things

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
 //Add an alert to game.js and test that the alert gets triggered when you load up index.html in Chrome.
 // Add jQuery to your website and test that it's successfully loaded by opening Chrome developer tools and typing $("h1")
- //$("#level-title").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+ 
 const buttonColours = ["red","blue","green","yellow" ];
-const gamePattern = []
+const gamePattern = [];
+const userClickedPattern = [];
+let randomNumber;
+let randomChosenColour;
+
+
+
+
 function nextSequence(){
-
-    randomNumber;
-    console.log(randomNumber)
-}
-
-const randomNumber = Math.floor(Math.random() * 3) + 1
-const randomChosenColour = buttonColours[randomNumber]
-//
-
-//  $(document).click(function() {  
-//      // Select and manipulate the button using jQuery
-//     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-//   });
+    
+randomNumber = Math.floor(Math.random() * 3) + 1;
+randomChosenColour = buttonColours[randomNumber];
 
 
-// $(document).ready(function(){
-//     $("#" + randomChosenColour).click(function(){
-//         $(this).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-       
-//     })
-// })
-
+//jquery section
 jQuery(document).ready(function() {
     jQuery(document).keypress(function() {
         jQuery("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+        jQuery("#" + randomChosenColour)
+       playSound(`${randomChosenColour}`);
     });
 });
 
+
+//when btn is clicked
+$(document).ready(function() {
+    var id;  
+    $('.btn').click(function() {
+
+     id=$(this).attr('id');
+     animatePress();
+      playSound(id);
+    
+    });
+  });
+  
+//busy section
+//busy section
+//busy section
 
 $(document).ready(function() {
-    $(document).keypress(function() {
-        $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    });
+  var userChosenColour; // Variable to store the ID of the clicked button
+
+  $('.btn').click(function() {
+    // Handler function for button clicks
+    userChosenColour = $(this).attr('id'); // Store the ID of the clicked button
+    userClickedPattern.push(userChosenColour);
+   
+    
+  });
 });
 
-
-// alert(randomChosenColour)
 gamePattern.push(randomChosenColour)
 
-// Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+}
+
+
+nextSequence();
+
+
+function playSound(name){
+   var audio = new Audio(`${name}.mp3`);
+   audio.play();
+ }
+
+
+
+ function animatePress(currentColour){
+
+
+        $(".btn").click(function(){
+          $(currentColour).addClass("pressed")
+             console.log(currentColour)
+        })
+
+
+ }
